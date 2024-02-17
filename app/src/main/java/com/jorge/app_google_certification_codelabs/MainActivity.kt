1452df17ef834bc7b57da88751a7576986050263
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-02-17 19:15:06 -0400

    Feat - Implementação do layout inicial

--- app/src/main/java/com/jorge/app_google_certification_codelabs/MainActivity.kt
+++ app/src/main/java/com/jorge/app_google_certification_codelabs/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.jorge.app_google_certification_codelabs
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jorge.app_google_certification_codelabs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
